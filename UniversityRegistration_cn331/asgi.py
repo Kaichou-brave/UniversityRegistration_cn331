b5c255df86dc4abd4b3e1c55f05a9da4934bb283
Raned Chuphueak <6210612864@student.tu.ac.th>
2021-09-08 00:07:51 +0700

    add initial files

---
+++ UniversityRegistration_cn331/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for UniversityRegistration_cn331 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UniversityRegistration_cn331.settings')
+
+application = get_asgi_application()
