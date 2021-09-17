425362acac3d407dec249835f1b6bdd5b7351aa9
raned.chu <6210612864@student.tu.ac.th>
2021-09-17 23:51:32 +0700

    create profile models in user app may be in future this project will have a profile page for each student that staff can access to viewwhat course do student enroll and how many credit they've got

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    username = models.CharField(max_length=200, blank=True, null=True)
+    fname = models.CharField(max_length=200, blank=True, null=True)
+    lname = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=500, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(blank=True, null=True, upload_to='profiles/', default='profiles/user-default.png')
+
+    def __str__(self):
+        return str(self.username)
