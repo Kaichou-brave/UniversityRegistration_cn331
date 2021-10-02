13cb017e72abba8b5881b74aa8617b932d3088a9
raned.chu <6210612864@student.tu.ac.th>
2021-10-02 12:33:10 +0700

    edit app_name so need to edit url in template too

--- courses/urls.py
+++ courses/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "courses"
 urlpatterns = [
     path('registration', views.registration, name='registration'),
     path('courses', views.courses, name='courses'),
